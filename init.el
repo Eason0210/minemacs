fc0f8435059680816fc3f99f1698be51d6fbdeb1
Eason0210 <aqua0210@163.com>
2025-06-28 21:55:59 +0800

    Setup cc-mode

--- init.el
+++ init.el
@@ -699,6 +699,53 @@
                                     (treesit-available-p)))
   (rust-ts-flymake-command nil))
 
+(use-package cc-mode
+  :defer t
+  :custom (backward-delete-char-untabify-method nil)
+  :config
+  (add-to-list 'c-default-style '(c++-mode . "linux-kernel"))
+  (add-to-list 'c-default-style '(c-mode . "linux-kernel"))
+  :preface
+  (defun c-lineup-arglist-tabs-only (ignored)
+    "Line up argument lists by tabs, not spaces"
+    (let* ((anchor (c-langelem-pos c-syntactic-element))
+           (column (c-langelem-2nd-pos c-syntactic-element))
+           (offset (- (1+ column) anchor))
+           (steps (floor offset c-basic-offset)))
+      (* (max steps 1)
+         c-basic-offset)))
+  (c-add-style
+   "linux-kernel"
+   '((c-basic-offset . 8)
+     (c-label-minimum-indentation . 0)
+     (c-offsets-alist . (
+                         (arglist-close         . c-lineup-arglist-tabs-only)
+                         (arglist-cont-nonempty . (c-lineup-gcc-asm-reg
+                                                   c-lineup-arglist-tabs-only))
+                         (arglist-intro         . +)
+                         (brace-list-intro      . +)
+                         (c                     . c-lineup-C-comments)
+                         (case-label            . 0)
+                         (comment-intro         . c-lineup-comment)
+                         (cpp-define-intro      . +)
+                         (cpp-macro             . -1000)
+                         (cpp-macro-cont        . +)
+                         (defun-block-intro     . +)
+                         (else-clause           . 0)
+                         (func-decl-cont        . +)
+                         (inclass               . +)
+                         (inher-cont            . c-lineup-multi-inher)
+                         (knr-argdecl-intro     . 0)
+                         (label                 . -1000)
+                         (statement             . 0)
+                         (statement-block-intro . +)
+                         (statement-case-intro  . +)
+                         (statement-cont        . +)
+                         (substatement          . +)
+                         ))
+     (indent-tabs-mode . t)
+     (show-trailing-whitespace . t))))
+
 ;;; Built-in packages
 
 (use-package eldoc
