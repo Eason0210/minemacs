2e41d5828ea77f239b10d711c18809a0e0e6af5b
Eason0210 <aqua0210@163.com>
2025-06-28 22:42:27 +0800

    Add writeroom-mode and define prose-mode for org-mode

--- init.el
+++ init.el
@@ -656,6 +656,49 @@
        (tags "CLOSED>=\"<today>\""
              ((org-agenda-overriding-header "Completed today"))))))))
 
+;; Writing mode similar to the famous Writeroom editor for macOS
+(use-package writeroom-mode
+  :vc (:url "https://github.com/joostkremers/writeroom-mode" :rev :latest)
+  :hook (org-mode . prose-mode)
+  :custom
+  (writeroom-fullscreen-effect 'maximized)
+  (writeroom-mode-line-toggle-position 'mode-line-format)
+  :preface
+  (define-minor-mode prose-mode
+    "Set up a buffer for prose editing.
+This enables or modifies a number of settings so that the
+experience of editing prose is a little more like that of a
+typical word processor."
+    :init-value nil :lighter " Prose" :keymap nil
+    (if prose-mode
+        (progn
+          (when (fboundp 'writeroom-mode)
+            (writeroom-mode 1))
+          (setq truncate-lines nil)
+          (setq word-wrap t)
+          (setq word-wrap-by-category t)
+          (setq cursor-type 'bar)
+          (when (eq major-mode 'org)
+            (kill-local-variable 'buffer-face-mode-face))
+          (buffer-face-mode 1)
+          (setq-local blink-cursor-interval 0.6)
+          (setq-local show-trailing-whitespace nil)
+          ;; (setq-local line-spacing 0.2)
+          (setq-local electric-pair-mode nil)
+          (visual-line-mode 1))
+      (kill-local-variable 'truncate-lines)
+      (kill-local-variable 'word-wrap)
+      (kill-local-variable 'word-wrap-by-category)
+      (kill-local-variable 'cursor-type)
+      (kill-local-variable 'blink-cursor-interval)
+      (kill-local-variable 'show-trailing-whitespace)
+      ;; (kill-local-variable 'line-spacing)
+      (kill-local-variable 'electric-pair-mode)
+      (buffer-face-mode -1)
+      (visual-line-mode -1)
+      (when (fboundp 'writeroom-mode)
+        (writeroom-mode 0)))))
+
 (use-package markdown-mode
   :ensure t
   :bind (:map markdown-mode-map
